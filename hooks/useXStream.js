3ed60528387b374e280ce3bc6089076e14498ecf
Aditya <aditya26sg@gmail.com>
2023-03-25 17:19:39 +0530

    fetching balance. calc flowrate

--- hooks/useXStream.js
+++ hooks/useXStream.js
@@ -36,7 +36,51 @@ const useXStream = () => {
   const [receipient, setReceipient] = useState();
   const [testFlowRate, setTestFlowRate] = useState(0);
 
-  
+  const { chain } = getNetwork();
 
+  const currentDate = new Date();
+  const hoursToAdd = 6;
+  currentDate.setTime(currentDate.getTime() + hoursToAdd * 60 * 60 * 1000);
+  const [endDate, setEndDate] = useState(dayjs(currentDate));
+
+  const getBalance = async (tokenAddress) => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const tokenBalance = await fetchBalance({
+          address: address,
+          token: tokenAddress,
+        });
+        console.log("the current selected tokenbalance is ", tokenBalance);
+        setBalance(tokenBalance.formatted);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (token?.address) {
+      console.log("Getting token balance of ", token.address);
+      getBalance(token?.address);
+    }
+  }, [token?.address, chain?.id]);
+
+  const calculateFlowRate = (amountInEther) => {
+    const now = new Date();
+
+    const endD = new Date(endDate);
+    const timeDiff = Math.abs(endD.getTime() - now.getTime());
+    console.log(timeDiff);
+
+    const amount = ethers.utils.parseEther(amountInEther.toString());
+    const calculatedFlowRate = Math.floor(amount / timeDiff);
+
+    // alert(calculatedFlowRate);
+
+    return calculatedFlowRate;
+  };
+
+  
 };
 export default useXStream;
