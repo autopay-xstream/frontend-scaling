866ce3402cb8329e3adb5cf0f30385e6590bd532
Aditya <aditya26sg@gmail.com>
2023-03-26 02:17:23 +0530

    initializing wagmi client

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,56 @@
-import '@/styles/globals.css'
+import Layout from "../../components/Layout";
+import {
+  getDefaultWallets,
+  lightTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { goerli, polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import "../styles/container.css";
+import "../styles/dashboard.css";
+import "../styles/globals.css";
+import "../styles/setpermission.css";
+import AuthProvider from "../../providers/AuthProvider";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { chains, provider } = configureChains(
+    [polygonMumbai, goerli],
+    [publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "Autopay",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={lightTheme({
+            accentColor: "#10bb35",
+            accentColorForeground: "white",
+            borderRadius: "medium",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          <AuthProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
+  );
 }
